27b0764b8a3d763aca676b7fa8864edb1754508f
Youp Bronkhorst <iot@ybronkhorst.nl>
2026-07-13 15:42:16 +0200

    Fix indentation in calendar index view

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -13,7 +13,7 @@
             </h1>
         </div>
 
-        <a href="{{ route('activities.create', ['trip' => $selectedTripId]) }}">
+        <a href="{{ route('activities.create', ['trip' => $selectedTripId]) }}"
             class="inline-flex items-center justify-center rounded-xl bg-blue-700 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-800" >
             + Activiteit</a>
     </div>
